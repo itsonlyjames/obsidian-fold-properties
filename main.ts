455b5b7b1b85ba558e81400085e758e4cb6a11af
itsonlyjames <jamesalexandre1995@gmail.com>
2024-09-28 11:56:52 +1000

    Init Fold/Unfold Functionality on Folder Context Menu

--- main.ts
+++ main.ts
@@ -1,134 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Menu, Notice, Plugin, TFile, TFolder } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
+export default class FoldProperties extends Plugin {
+    async onload() {
+        this.registerEvent(
+            this.app.workspace.on(
+                'file-menu',
+                (menu: Menu, file: TFile | TFolder) => {
+                    if (file instanceof TFolder && file.children.length !== 0) {
+                        menu.addItem((item) => {
+                            item.setTitle('Fold Properties')
+                                .setIcon('fold')
+                                .onClick(async () => {
+                                    await this.foldAllProperties(file)
+                                })
+                        })
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+                        menu.addItem((item) => {
+                            item.setTitle('Unfold Properties')
+                                .setIcon('unfold')
+                                .onClick(async () => {
+                                    await this.unfoldAllProperties(file)
+                                })
+                        })
+                    }
+                }
+            )
+        )
+    }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    async foldAllProperties(folder: TFolder) {
+        for (const child of folder.children) {
+            if (child instanceof TFile) {
+                await this.foldPropertiesForFile(child)
+            } else if (child instanceof TFolder) {
+                await this.foldAllProperties(child)
+            }
+        }
+        new Notice(`Folded properties for all files in ${folder.path}`)
+    }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+    async unfoldAllProperties(folder: TFolder) {
+        for (const child of folder.children) {
+            if (child instanceof TFile) {
+                await this.unfoldPropertiesForFile(child)
+            } else if (child instanceof TFolder) {
+                await this.unfoldAllProperties(child)
+            }
+        }
+        new Notice(`Unfolded properties for all files in ${folder.path}`)
+    }
 
-	async onload() {
-		await this.loadSettings();
+    async foldPropertiesForFile(file: TFile) {
+        const content = await this.app.vault.read(file)
+        const lines = content.split('\n')
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        const folds = {
+            folds: [{ from: 0, to: 0 }],
+            lines: lines.length
+        }
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        this.app.foldManager.savePath(file.path, folds)
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    async unfoldPropertiesForFile(file: TFile) {
+        const existingFolds = this.app.foldManager.loadPath(file.path)
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        if (existingFolds) {
+            const path = this.app.appId + '-note-fold-' + file.path
+            localStorage.removeItem(path)
+        }
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    onunload() {}
 }
